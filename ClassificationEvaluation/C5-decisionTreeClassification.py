f02e8ce6a4f55440c025d85fc647df060f6f72d6
Mahnoor Anjum <manomaq@gmail.com>
2020-05-24 07:56:47 +0500

    updated repository

---
+++ ClassificationEvaluation/C5-decisionTreeClassification.py
@@ -0,0 +1,112 @@
+##############################################################################
+#
+#    Mahnoor Anjum
+#    manomaq@gmail.com
+#    References:
+#        SuperDataScience,
+#        Official Documentation
+#
+#
+##############################################################################
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+def plot_map(X_set, y_set, classifier, text):
+    X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                         np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+    plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+                 alpha = 0.75, cmap = ListedColormap(('pink', 'cyan','cornflowerblue')))
+    plt.xlim(X1.min(), X1.max())
+    plt.ylim(X2.min(), X2.max())
+    plt.title(text)
+    plt.xlabel('X1')
+    plt.ylabel('X2')
+    plt.legend()
+    plt.show()
+    plt.clf()
+    
+def plot_model(X_set, y_set, classifier, text):
+    X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                         np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+    plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+                 alpha = 0.75, cmap = ListedColormap(('pink', 'cyan','cornflowerblue')))
+    plt.xlim(X1.min(), X1.max())
+    plt.ylim(X2.min(), X2.max())
+    for i, j in enumerate(np.unique(y_set)):
+        plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                    c = ListedColormap(('red', 'blue','midnightblue'))(i), label = j)
+    plt.title(text)
+    plt.xlabel('X1')
+    plt.ylabel('X2')
+    plt.legend()
+    plt.show()
+    plt.clf()
+    
+def evaluate(y_test,y_pred):
+    from sklearn.metrics import accuracy_score
+    accuracy = accuracy_score(y_test, y_pred)
+    print("============================================================")
+    print("Accuracy Score: " + str(accuracy))
+    # Getting evaluation report
+    from sklearn.metrics import classification_report
+    print("============================================================")
+    print(classification_report(y_test, y_pred))
+    print("============================================================")
+
+
+# Importing the dataset
+# iloc gets data via numerical indexes
+# .values converts from python dataframe to numpy object
+dataset = pd.read_csv('Moons.csv')
+X = dataset.iloc[:, 1:3].values
+y = dataset.iloc[:, 3].values
+
+from matplotlib.colors import ListedColormap
+for i, j in enumerate(np.unique(y)):
+    plt.scatter(X[y == j, 0], X[y == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Dataset')
+plt.xlabel('X1')
+plt.ylabel('X2')
+plt.legend()
+plt.show()
+plt.clf()
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+
+#============================== DECISION TREES ===================================
+#    
+#    criterion : string, optional (default=”gini”)
+#    The function to measure the quality of a split. 
+#    Supported criteria are “gini” for the Gini impurity and 
+#    “entropy” for the information gain.
+#    
+#    splitter : string, optional (default=”best”)
+#    The strategy used to choose the split at each node. Supported strategies are 
+#    “best” to choose the best split and “random” to choose the best random split.
+#    
+#    max_depth : int or None, optional (default=None)
+#    The maximum depth of the tree. If None, then nodes are expanded until all 
+#    leaves are pure or until all leaves contain less than min_samples_split samples.
+#
+
+
+# Fitting Decision Tree Classification to the Training set
+from sklearn.tree import DecisionTreeClassifier
+classifier = DecisionTreeClassifier(criterion = 'entropy')
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+
+evaluate(y_test, y_pred)
+plot_map(X_train, y_train, classifier, 'DecisionTreeClassifier Boundary')
+plot_model(X_train, y_train, classifier, 'DecisionTreeClassifier Train')
+plot_model(X_test, y_test, classifier, 'DecisionTreeClassifier Test')
+
+from sklearn.metrics import plot_confusion_matrix
+plot_confusion_matrix(classifier, X_test, y_test,cmap=plt.cm.Blues)
